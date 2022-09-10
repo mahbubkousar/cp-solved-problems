eefc053c6113ff36f0836afcc38f8923cc56ffd3
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 18:32:06 +0600

    Create  1114 - Fixed Password.c

---
+++ beecrowd/1114 - Fixed Password.c
@@ -0,0 +1,19 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1114
+//Mahbub Kousar
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    while (n !=2002)
+
+    {
+        scanf("%d", &n);
+        if (n !=2002)
+        printf("Senha Invalida\n");
+        else
+        printf("Acesso Permitido\n");
+
+    }
+    return 0;
+}

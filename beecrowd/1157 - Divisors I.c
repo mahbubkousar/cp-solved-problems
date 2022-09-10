e88aa2110ddef07fc78b2d58d804f9df59575bf0
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 18:59:01 +0600

    Create  1157 - Divisors I.c

---
+++ beecrowd/1157 - Divisors I.c
@@ -0,0 +1,19 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1157
+//Mahbub Kousar
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+
+    for(i=1;i<=a;i++)
+    {
+
+        if(a%i==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+
+}

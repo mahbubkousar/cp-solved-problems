df9c10ddf4d4aa9fe1dc87aad285db6f547f7cfa
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:02:12 +0600

    Create 1001.c

---
+++ beecrowd/1001.c
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1001
+//bee-1001
+#include <stdio.h>
+ 
+
+ 
+int main() {
+ 
+    int a, b;
+    scanf("%d",&a);
+     scanf("%d",&b);
+     printf("X = %d\n", a+b);
+    return 0;
+}

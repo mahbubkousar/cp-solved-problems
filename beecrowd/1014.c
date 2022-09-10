73086fd9857587f2ed2106ab00e8e4e886e8b538
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:21:52 +0600

    Create 1014.c

---
+++ beecrowd/1014.c
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1014
+//1014 - Consumption
+//Mahbub Kousar
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    scanf("%d",&a);
+    scanf("%f",&b);
+    printf("%.3lf km/l\n", a/b);
+    return 0;
+}

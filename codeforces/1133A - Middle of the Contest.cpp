07b578ca6a383c18374f9c49e344bda218b0964d
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 22:27:05 +0600

    Create 1133A - Middle of the Contest.cpp

---
+++ codeforces/1133A - Middle of the Contest.cpp
@@ -0,0 +1,20 @@
+//https://codeforces.com/contest/1133/problem/A
+//Mahbub Kousar
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int a,b,c,d;
+    scanf("%d:%d",&a,&b);
+    scanf("%d:%d",&c,&d);
+    int t=(((c*60)+d)-((a*60)+b))/2;
+    int dh=t/60,dm=t%60;
+    if(dm+b >=60) {
+        b -= 60; a+=1;
+    }
+    printf("%02d:%02d\n",a+dh,b+dm);
+    return 0;
+}

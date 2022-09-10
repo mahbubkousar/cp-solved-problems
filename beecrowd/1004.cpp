cebc567adf7c77fb48cc1847cfe2dd852935023e
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:08:45 +0600

    Create 1004.cpp

---
+++ beecrowd/1004.cpp
@@ -0,0 +1,13 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1004
+//bee-1004
+//Mahbub Kousar
+#include <iostream>
+using namespace std;
+int main() {
+ 
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("PROD = %d\n",a*b);
+    return 0;
+}

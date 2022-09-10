1184d748304000392cc4664bd1103143e0869ce3
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:13:29 +0600

    Create 1008.c

---
+++ beecrowd/1008.c
@@ -0,0 +1,18 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1008
+//1008 - Salary
+//Mahbub Kousar
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a,b;
+    float c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%f", &c);
+    printf("NUMBER = %d\n",a);
+    printf("SALARY = U$ %.2f\n",b*c);
+    
+ 
+    return 0;
+}

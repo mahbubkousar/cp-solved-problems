bb16ad72ad29c607ee914d5264dd19bd8191b8ab
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:58:59 +0600

    Create  1048 - Salary Increase.c

---
+++ beecrowd/1048 - Salary Increase.c
@@ -0,0 +1,42 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1048
+//1048 - Salary Increase
+//Mahbub Kousar
+#include <stdio.h>
+
+int main()
+{
+    float s;
+    scanf("%f", &s);
+    if (s>0 && s<= 400.00)
+    {
+        printf("Novo salario: %.2f\n", s+(s*.15));
+        printf("Reajuste ganho: %.2f\n",s*.15);
+        printf("Em percentual: 15 %%\n");
+    }
+    else if (s<= 800.00)
+    {
+        printf("Novo salario: %.2f\n", s+(s*.12));
+        printf("Reajuste ganho: %.2f\n",s*.12);
+        printf("Em percentual: 12 %%\n");
+    }
+    else if (s<= 1200.00)
+    {
+        printf("Novo salario: %.2f\n", s+(s*.10));
+        printf("Reajuste ganho: %.2f\n",s*.10);
+        printf("Em percentual: 10 %%\n");
+    }
+    
+    else if (s<= 2000.00)
+    {
+        printf("Novo salario: %.2f\n", s+(s*.07));
+        printf("Reajuste ganho: %.2f\n",s*.07);
+        printf("Em percentual: 7 %%\n");
+    }
+    else 
+       {
+        printf("Novo salario: %.2f\n", s+(s*.04));
+        printf("Reajuste ganho: %.2f\n",s*.04);
+        printf("Em percentual: 4 %%\n");
+    } 
+    return 0;
+}

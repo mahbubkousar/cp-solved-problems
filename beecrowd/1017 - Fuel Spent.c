e24026a7ae17a15807cef16f59bd24929877e5b5
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:26:10 +0600

    Create  1017 - Fuel Spent.c

---
+++ beecrowd/1017 - Fuel Spent.c
@@ -0,0 +1,12 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1017
+//1017 - Fuel Spent
+//Mahbub Kousar
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a, &b);
+    printf("%.3lf\n", (a*b)/12.0);
+    return 0;
+}

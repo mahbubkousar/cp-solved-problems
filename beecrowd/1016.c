ee4ba497b091f93122be0bd02d29ad21edbf9f70
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:24:57 +0600

    Create 1016.c

---
+++ beecrowd/1016.c
@@ -0,0 +1,12 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1016
+//1016 - Distance
+//Mahbub Kousar
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d minutos\n", a*2);
+    return 0;
+}

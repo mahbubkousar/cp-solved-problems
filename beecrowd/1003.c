8cdad1b02534519245739904132d42015aa0d2a8
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:06:44 +0600

    Create 1003.c

---
+++ beecrowd/1003.c
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1003
+//bee-1003
+//Mahbub Kousar
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("SOMA = %d\n",a+b);
+ 
+    return 0;
+}

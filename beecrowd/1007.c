fddbb607e2bff9a75647b2b95527d2021d503363
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:12:29 +0600

    Create 1007.c

---
+++ beecrowd/1007.c
@@ -0,0 +1,15 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1007
+//1007 - Difference
+//Mahbub Kousar
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    scanf("%d",&d);
+    printf("DIFERENCA = %d\n", a*b-c*d);
+    return 0;
+}

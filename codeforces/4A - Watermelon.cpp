745b2aeca52382e4a1205866bce9359691759a6b
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 23:16:42 +0600

    Create 4A - Watermelon.cpp

---
+++ codeforces/4A - Watermelon.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/contest/4/problem/A
+//Mahbub Kousar
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n>2 && n%2 == 0)
+        cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}

6d2395bea4ed27ba5826cd2034f3d206b0bf0655
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 22:57:23 +0600

    Create 50A - Domino piling.cpp

---
+++ codeforces/50A - Domino piling.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/contest/50/problem/A
+//Mahbub Kousar
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin >> m>> n;
+    cout << (m*n)/2 << endl;
+ 
+    return 0;
+}

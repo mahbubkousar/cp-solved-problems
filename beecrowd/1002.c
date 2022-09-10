70767a960b5339357a39e9e01b2f75a4bbf3a11a
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:05:36 +0600

    Create 1002.c

---
+++ beecrowd/1002.c
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1002
+//bee-1002
+//Mahbub Kousar
+#include <stdio.h>
+ #define pi  3.14159
+int main() {
+ 
+    double a,b;
+    scanf("%lf",&a);
+    b=pi *a*a;
+    printf("A=%.4lf\n",b);
+ 
+    return 0;
+}

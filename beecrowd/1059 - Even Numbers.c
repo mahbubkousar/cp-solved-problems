2203218b013ea232e9b5df6455beb7b5640f03da
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 18:04:41 +0600

    Create  1059 - Even Numbers.c

---
+++ beecrowd/1059 - Even Numbers.c
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1059
+//1059 - Even Numbers
+//Mahbub Kousar
+#include <stdio.h>
+
+int main() {
+  int n;
+
+  for (n = 2; n <=100 ; n=n+2)
+  {
+    printf("%d\n", n);
+  }
+  return 0;
+}

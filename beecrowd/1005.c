34f4674baf0fd0a4697eb172663eab6e504e23f1
mahbubkousar <mahbubkousar@gmail.com>
2022-09-10 17:09:47 +0600

    Create 1005.c

---
+++ beecrowd/1005.c
@@ -0,0 +1,13 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1005
+//bee-1005
+//Mahbub Kousar
+#include <stdio.h>
+ 
+int main() {
+ 
+    float a,b;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    printf("MEDIA = %.5f\n", (a*3.5+b*7.5)/(3.5+7.5));
+    return 0;
+}
